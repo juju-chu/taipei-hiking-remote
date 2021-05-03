de2fdf93a96be4c487b7ed92d7b7f551c6b14148
Juju <jujupp1116@gmail.com>
2021-05-03 19:36:54 +0800

    feat: fix navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,20 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Navbar } from './../../node_modules/react-bootstrap'
 
+const headerStyle = {
+  zIndex: 1,
+  width: '100vw',
+}
+
 const Header = () => {
   return (
-    <>
+    <div className="header position-fixed" style={headerStyle}>
       <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary">
         <a className="navbar-brand" href="/">
           台北市健行步道
         </a>
       </Navbar>
-    </>
+    </div>
   )
 }
 
