44e0b0fa4cf0582d2e758f7fc3038cce71411316
Juju <jujupp1116@gmail.com>
2021-05-03 14:35:22 +0800

    feat: project init

---
+++ src/Hiking.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import Paths from './components/Paths'
+import Pagination from './components/Pagination'
+
+const Hiking = () => {
+  return (
+    <>
+      <Header></Header>
+      <Paths></Paths>
+      <Pagination></Pagination>
+    </>
+  )
+}
+
+export default Hiking
---
+++ src/components/Paths.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Paths = () => {
+  return <h1>Paths</h1>
+}
+
+export default Paths
