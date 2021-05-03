5541840c8b8f2b0c31b61c823f7b979f9379cc5d
Juju <jujupp1116@gmail.com>
2021-05-03 17:38:54 +0800

    feat: add navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Navbar } from './../../node_modules/react-bootstrap'
 
 const Header = () => {
-  return <h1>Header</h1>
+  return (
+    <>
+      <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href="/">
+          台北市健行步道
+        </a>
+      </Navbar>
+    </>
+  )
 }
 
 export default Header
