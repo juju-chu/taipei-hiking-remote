ca2dcf9a084cef40954728bcc2d4595946f2f021
Juju <jujupp1116@gmail.com>
2021-05-06 15:27:01 +0800

    feat: add modal

--- src/components/Path.js
+++ src/components/Path.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ClampLines from 'react-clamp-lines'
+import { Modal, Button } from './../../node_modules/react-bootstrap'
 
 const cardStyle = {
   maxWidth: '15rem',
 }
 
 const Path = ({ id, name, district, description }) => {
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
   return (
     <div className="path">
       <div className="card border-danger m-3" style={cardStyle}>
         <div className="card-header">{district}</div>
-        <div className="card-body">
+        <div className="card-body" onClick={handleShow}>
           <h4 className="card-title">{name}</h4>
           <ClampLines
             text={description}
@@ -25,6 +31,18 @@ const Path = ({ id, name, district, description }) => {
           />
         </div>
       </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{description}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            關閉
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
